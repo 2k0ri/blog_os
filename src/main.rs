641271757e1ed1ca68d5f60c9117371dd21b6e0c
2k0ri <esc13245@gmail.com>
2019-08-19 03:20:27 +0900

    VGA Text Mode; Hello World using println

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,19 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-mod vga_buffer;
-
 #[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -92,14 +92,22 @@ impl Writer {
         for row in 1..BUFFER_HEIGHT {
             for col in 0..BUFFER_WIDTH {
                 let character = self.buffer.chars[row][col].read();
-                self.buffer.chars[row - 1][col].write(character)
+                self.buffer.chars[row - 1][col].write(character);
             }
         }
         self.clear_row(BUFFER_HEIGHT - 1);
         self.column_position = 0;
     }
 
-    fn clear_row(&mut self, row: usize) {/* TODO */}
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 use core::fmt;
@@ -122,4 +130,21 @@ lazy_static! {
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
